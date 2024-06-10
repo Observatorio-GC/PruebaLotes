39df71babfc7d8a7315d1fb61ac27f34c245ebcf
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-06-10 11:37:15 -0300

    Add files via upload

---
+++ scripts/LotePrueba.js
@@ -0,0 +1,8 @@
+var LotePrueba ={
+"type": "FeatureCollection",
+"name": "Lote-Prueba",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "padron": 4722, "direccion": "Belgrano 404", "sup": "4831 m2", "zonific": "Residencial Mixta" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.844469593099021, -32.919703912933095 ], [ -68.843748895896439, -32.919848715721542 ], [ -68.843908913421956, -32.920465295583448 ], [ -68.844619124702959, -32.920328323238536 ], [ -68.844469593099021, -32.919703912933095 ] ] ] ] } }
+]
+}
---
+++ scripts/precio71278.js
@@ -0,0 +1,8 @@
+var precio71278 ={
+"type": "FeatureCollection",
+"name": "precio71278-80000",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 65, "Precio": 973952.0, "Comentario": null, "PrecioUSD": "936" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.845817321867955, -32.925876413586202 ], [ -68.845199355811445, -32.92430033457385 ], [ -68.842988938763199, -32.92473924547371 ], [ -68.843606904819694, -32.926355216840093 ], [ -68.84560341054069, -32.925936264134648 ], [ -68.845817321867955, -32.925876413586202 ] ] ] ] } }
+]
+}
